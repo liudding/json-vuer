e918a1834a7f8c775126c9f7881c075a36a211f9
ding <ding@catMac.local>
2020-11-06 21:18:50 +0800

    editor styles

--- src/data-field-editor.vue
+++ src/data-field-editor.vue
@@ -1,45 +1,120 @@
 <template>
-  <div  class="data-field-editor">
+  <div class="data-field-editor">
     <form>
-      <label>name</label>
-      <input type="text" v-model="kv.name" />
+      <div class="form-group">
+        <label>name</label>
+        <input type="text" v-model="kv.name" />
+      </div>
 
-      <select>
-        <option value="string">string</option>
-      </select>
+      <div class="form-group">
+        <label>type</label>
 
-      <label>value</label>
-      <textarea v-model="kv.value"></textarea>
+        <select>
+          <option v-for="item in dataTypes" :key="item" value="string">{{ item }}</option>
+        </select>
+      </div>
 
-      <button>submit</button>
+        <div class="form-group">
+        <label>value</label>
+        <textarea v-model="kv.value"></textarea>
+      </div>
+
+      <div class="form-group">
+        <label></label>
+        <button>submit</button>
+      </div>
     </form>
   </div>
 </template>
 
 <script>
+// import DataType from './types'
 export default {
   model: {
-    prop: 'kv',
-    event: 'update'
+    prop: "kv",
+    event: "update",
   },
   props: {
     kv: Object,
   },
   data: () => {
-    return {};
+    return {
+      dataTypes: ['string', 'integer', 'boolean', 'null'],
+    };
+  },
+  created() {
   },
   methods: {
     submite() {
-      this.$emit('update', this.kv)
+      this.$emit("update", this.kv);
     },
   },
 };
 </script>
 
-<style>
-
+<style lang="scss" scoped>
 .data-field-editor {
-    background-color: white;
-}
+  background-color: white;
 
+  padding: 16px;
+
+  form {
+    position: relative;
+    z-index: 1;
+    background: #ffffff;
+    max-width: 360px;
+    margin: 0 auto 100px;
+    padding: 45px;
+    text-align: center;
+
+    .form-group {
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+
+      margin-bottom: 12px;
+
+      label {
+        width: 80px;
+        height: 100%;
+
+      }
+
+      select,
+      textarea,
+      input {
+        outline: 0;
+        background: #f2f2f2;
+        width: 100%;
+        border: 0;
+        padding: 12px 16px;
+        box-sizing: border-box;
+        font-size: 14px;
+      }
+
+      textarea {
+        resize: vertical;
+      }
+    }
+
+    button {
+      outline: 0;
+      background: #4caf50;
+      width: 100%;
+      border: 0;
+      padding: 15px;
+      color: #ffffff;
+      font-size: 14px;
+      -webkit-transition: all 0.3 ease;
+      transition: all 0.3 ease;
+      cursor: pointer;
+
+      &:hover,
+      &:active,
+      &:focus {
+        background: #43a047;
+      }
+    }
+  }
+}
 </style>
