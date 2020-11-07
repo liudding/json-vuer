1736583dcc766c7198697aa48e6f28b7501a2f48
liuding <liuding.ld@foxmail.com>
2020-11-07 16:10:47 +0800

    copy status

---
+++ src/icons/clipboard-copied.vue
@@ -0,0 +1,27 @@
+
+<script>
+export default {
+  render() {
+    return (
+      <svg
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        stroke-width="2"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        class="json-viewer-icon"
+      >
+        <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2"></path>
+        <rect x="8" y="2" width="8" height="4" rx="1" ry="1"></rect>
+        <polyline points="9 12 11 14 15 10"></polyline>
+      </svg>
+    );
+  },
+};
+</script>
+
+<style>
+</style>
--- src/toolbar/copy.vue
+++ src/toolbar/copy.vue
@@ -5,16 +5,27 @@
     class="json-node-tool json-node-tool__copy"
     title="Copy"
   >
-    <clipboard></clipboard>
+    <clipboard-copied
+      v-if="copied"
+      class="json-node-tool__copy__copied"
+    ></clipboard-copied>
+    <clipboard v-else class="json-node-tool__copy"></clipboard>
   </span>
 </template>
 
 <script>
 import Clipboard from "../icons/clipboard";
+import ClipboardCopied from "../icons/clipboard-copied";
 
 export default {
   props: ["value", "indentWidth"],
-  components: { Clipboard },
+  components: { Clipboard, ClipboardCopied },
+  data() {
+    return {
+      copied: false,
+      timer: null,
+    };
+  },
 
   methods: {
     checkCopySupported() {
@@ -37,6 +48,13 @@ export default {
       document.execCommand("copy");
 
       document.body.removeChild(elem);
+
+      clearTimeout(this.timer);
+
+      this.copied = true;
+      this.timer = setTimeout(() => {
+        this.copied = false;
+      }, 3000);
     },
 
     onClickCopy() {
